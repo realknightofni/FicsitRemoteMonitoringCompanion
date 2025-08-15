85538545e415435ee1164f4a85943d0a8a8ed703
realknightofni <realknightofni@gmail.com>
2025-08-15 18:50:31 -0500

    updated uobject metric name to avoid prom conflicts

--- Companion/exporter/uobject_metrics.go
+++ Companion/exporter/uobject_metrics.go
@@ -5,7 +5,7 @@ import "github.com/prometheus/client_golang/prometheus"
 // No extra labels; RegisterNewGaugeVec adds url and session_name automatically.
 var (
     UObjectCount = RegisterNewGaugeVec(prometheus.GaugeOpts{
-        Name: "uobject_count",
+        Name: "uobjects_loaded",
         Help: "Number of UObjects currently loaded",
     }, []string{})
 
